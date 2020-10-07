f2fcb456b4371d72c1dd97b669ad801107462033
Benjamin Soyka <bensoyka@icloud.com>
2020-10-07 09:14:47 -0600

    Version bump 0.4.1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyryver.util import retry_until_available
 
 from utils import Cooldown, TopicGenerator, bot_dir, console, send_message
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 load_dotenv(
     dotenv_path=bot_dir / ".env"
